e6107d47d4a7cbe0545e6579a25a66f406ba8684
oldkingOK <oldkingok@qq.com>
2024-05-15 16:40:00 +0800

    添加token输入用户界面

--- ok_yuketang.js
+++ ok_yuketang.js
@@ -7,27 +7,44 @@
 // @match        *://changjiang.yuketang.cn/*
 // @icon         https://changjiang.yuketang.cn/static/images/favicon.ico
 // @require      https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
-// @grant        none
-// @grant        unsafeWindow
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @run-at       document-end
 // ==/UserScript==
+
+function getToken() {
+    var v = GM_getValue("ok_token", "");
+    if (v == "") {
+        v = prompt("请输入Pushplus的token")
+        setToken(v)
+    }
+    return v
+}
+
+function setToken(token) {
+    GM_setValue("ok_token", token);
+}
 
 (function() {
     'use strict';
 
-    var token = '' // 这里改为pushplus的token
+    var token = getToken() // 这里改为pushplus的token
     var temp = console.log
-    console.log = function(args)
+
+    // 重写console.log
+    // https://stackoverflow.com/questions/54562790/cannot-set-property-which-only-has-getter-javascript-es6
+    console._log = function(args)
     {
         temp(args)
         if (args.op && (args.op == "probleminfo" || args.op == "unlockproblem")) {
+            var time = new Date().getTime() // 防止pushplus平台“频繁消息”
             $.ajax({
                 url: 'https://www.pushplus.plus/send',
                 type: 'GET',
                 data: {
                     'token': token,
-                    'title': '有新的答题了！',
-                    'content': '# 有新的答题了！',
+                    'title': '有新的答题了！' + time,
+                    'content': '# 有新的答题了！' + time,
                     'template': 'markdown'
                 },
                 success: function(data) {
